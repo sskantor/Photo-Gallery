e43b38559b45e712533dde47c68742baca684e13
sylwiakantor <44203473+sylwiakantor@users.noreply.github.com>
2021-03-18 17:26:30 +0100

    add files

---
+++ config.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = [
+    'db_host'     => 'localhost',
+    'db_name'     => 's35',
+    'db_user'     => 's35',
+    'db_password' => 't4q94vsx',
+	 'recaptcha_public' => '6LcrreYUAAAAAGeERM9Etdv8wlh81RCSK0LXuwfX',
+	 'recaptcha_private' => '6LcrreYUAAAAALTdoToeer_H4NZ1ECK4U76g0huL'
+];
+
+?>
+
